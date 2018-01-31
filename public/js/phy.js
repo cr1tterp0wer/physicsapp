9fe945eecd360273a8bbf9df45becfe3f7438fce
Critter <cr1tterp0wer@vps155727.vps.ovh.ca>
2018-01-31 09:26:46 -0500

    derp

--- public/js/phy.js
+++ public/js/phy.js
@@ -8,28 +8,61 @@ var smoothScroll = function(){ $('.smooth').on('click', function() {
      return false;
    });
 };
-smoothScroll();
+// smoothScroll();
 
 var backScroll = function(){
 
   $(document).scroll(function(){
-     if( $(document).scrollTop() >= $('#home').position().top ){
+    
+    var blur    = 0; 
+    var offset  = 0;
+    var dotP    = $('#dot-product')
+    var crossP  = $('#cross-product')
+    var dotPscope   = $('.dot-product01')
+    var crossPscope = $('.cross-product-triple') 
+
+    if( $(document).scrollTop() >= $('#home').position().top &&
+         $(document).scrollTop() <= $('#home').position().top + $('#home').height() ){
         $('body').removeClassRegEx(/\-back*/);
         $('body').addClass('gray-back');
         $('nav a').removeClass('active')
      }
-     if( $(document).scrollTop() >= $('#dot-product').position().top  ){
+     if( $(document).scrollTop() >= dotP.position().top  &&
+         $(document).scrollTop() <= dotP.position().top + dotP.height() ){
         $('body').removeClassRegEx(/\-back*/);
         $('body').addClass('fire-back');
         $('nav a').removeClass('active');
         $('nav a.dot-product').addClass('active');
+
+        offset =  dotPscope.height()/8;
+        if(blur<0)
+          blur=0;
+        else{
+          blur = dotPscope.position().top - $(document).scrollTop() - offset
+          dotPscope.css('filter','blur(' + (blur/offset) + 'px)');
+        }
      }
-     if( $(document).scrollTop() >= $('#cross-product').position().top  ){
+     if( $(document).scrollTop() >= crossP.position().top &&
+         $(document).scrollTop() <= crossP.position().top + crossP.height() ){
         $('body').removeClassRegEx(/\-back*/);
         $('body').addClass('ocean-back');
         $('nav a').removeClass('active')
         $('nav a.cross-product').addClass('active');
+
+
+        offset =  crossPscope.height()/8;
+        if(blur<0){
+          blur=0;
+        }
+        else{
+          blur = crossPscope.position().top - $(document).scrollTop() - offset
+         crossPscope.css('filter','blur(' + (blur/offset) + 'px)');
+        }
+     
+     
      }
+
+      //Menu
      if( $(document).scrollTop() >=(
        $('.masthead.mb-auto').position().top ) + $('.masthead.mb-auto').height()+50  ){
        $('nav.nav-masthead').addClass('fixed-top');
