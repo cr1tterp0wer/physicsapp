f67c01cc884d434cba449bb2d587cc56ba77064a
cr1tterp0wer <capodacac@gmail.com>
2019-11-15 00:51:40 -0600

    heroku changes

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app     = express();
 var path    = require('path');
 var http    = require('http'); //http server obj
+var port    = process.env.PORT || 8080;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 //define routes
 app.get('/',function(req,res){
@@ -12,7 +13,7 @@ app.get('/',function(req,res){
 });
 
 //spin up server
-var server = app.listen(app.get('port'),function(){
+var server = app.listen( port, function(){
 
   var port = server.address().port;
   console.log('Listening on port ' + port);
